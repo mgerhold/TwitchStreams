2cc16892e951b72d40f8c69e32e4c5efd669cb81
Michael Gerhold <michael.gerhold@web.de>
2021-04-16 17:13:04 +0200

    Added Clients.sql

---
+++ ModelViewControllerDemo/Clients.sql
@@ -0,0 +1,6 @@
+CREATE TABLE clients (
+ ClientID int(11) NOT NULL AUTO_INCREMENT,
+ FirstName varchar(255) NOT NULL,
+ LastName varchar(255) NOT NULL,
+ PRIMARY KEY (ClientID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
