f11e4d1ddb003bfb4dadc16be215f3a869a14fde
Michael Gerhold <michael.gerhold@web.de>
2021-06-18 21:58:06 +0200

    Changed program to use a mutex to protect a single result variable

--- Concurrency/main.cpp
+++ Concurrency/main.cpp
@@ -7,6 +7,7 @@
 #include <limits>
 #include <cstdint>
 #include <cmath>
+#include <mutex>
 
 std::string getCurrentThreadID() {
     std::stringstream ss;
@@ -14,13 +15,14 @@ std::string getCurrentThreadID() {
     return ss.str();
 }
 
-void sumPart(const auto begin, const auto end, auto &accumulator) {
-    auto tempAccumulator = accumulator;// local variable to prevent false sharing
+void sumPart(const auto begin, const auto end, std::mutex &accumulatorMutex, auto &accumulator) {
+    std::remove_reference_t<decltype(accumulator)> tempAccumulator{};// local variable to prevent false sharing
     for (auto it = begin; it != end; ++it) {
         tempAccumulator += static_cast<int>(std::cos(std::sqrt(*it) - 1.0));// additional computation to enhance
                                                                             // the observed effect
     }
-    accumulator = tempAccumulator;
+    std::scoped_lock lock(accumulatorMutex);
+    accumulator += tempAccumulator;
 
     // alternative code without local variable (shows significantly worse timing behavior):
     /*for (auto it = begin; it != end; ++it) {
@@ -41,38 +43,36 @@ void sumPart(const auto begin, const auto end, auto &accumulator) {
 
     const size_t numElementsPerThread = numElements / numThreads;
     std::vector<std::jthread> workerThreads;
-    std::vector<ValueType> partialSums(numThreads);
+    ValueType result{};
+    std::mutex resultMutex;
     auto rangeBegin = begin;
     for (unsigned int i = 0U; i < numThreads - 1U; ++i) {
         const auto previousRangeBegin = rangeBegin;
         std::advance(rangeBegin, numElementsPerThread);
         const auto rangeEnd = rangeBegin;
         workerThreads.emplace_back(sumPart<decltype(previousRangeBegin), decltype(rangeEnd), ValueType>,
-                                   previousRangeBegin, rangeEnd,
-                                   std::ref(partialSums.at(i)));// "stf::ref" because jthread's c'tor
-                                                                // takes arguments by value and copies
-                                                                // them (just like std::bind)
+                                   previousRangeBegin, rangeEnd, std::ref(resultMutex),
+                                   std::ref(result));// "std::ref" because jthread's c'tor
+                                                     // takes arguments by value and copies
+                                                     // them (just like std::bind)
     }
-    sumPart<decltype(rangeBegin), decltype(end), ValueType>(rangeBegin, end, partialSums.back());
+    sumPart<decltype(rangeBegin), decltype(end), ValueType>(rangeBegin, end, resultMutex, result);
     for (auto &thread : workerThreads) {
         thread.join();
     }
-    ValueType result{};
-    sumPart(partialSums.cbegin(), partialSums.cend(), result);
     return result;
 }
 
-
 int main() {
     using ValueType = int;
-    constexpr std::size_t numberOfInts = static_cast<std::size_t>(std::numeric_limits<int>::max());
+    constexpr auto numberOfInts = static_cast<std::size_t>(std::numeric_limits<int>::max());
     std::cout << "Creating vector...\n";
     std::vector<ValueType> intsVector(numberOfInts);
     std::cout << "Vector created...\n";
     std::fill(intsVector.begin(), intsVector.end(), 1);
     std::cout << "Vector filled...\n";
 
-    for (unsigned int numThreads = 1U; numThreads <= 48U; ++numThreads) {
+    for (unsigned int numThreads = 1U; numThreads <= 24U; ++numThreads) {
         const auto start = std::chrono::high_resolution_clock::now();
         [[maybe_unused]] const auto sumOfInts = sum(intsVector.cbegin(), intsVector.cend(), numThreads);
         const auto end = std::chrono::high_resolution_clock::now();
